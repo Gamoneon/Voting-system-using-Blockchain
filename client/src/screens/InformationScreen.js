9edc57d28202976f2ef2feb3b229e91dee01b827
Gamoneon <saurabh8796jadhav@gmail.com>
2022-05-11 10:19:47 +0530

    Information Screen Updated

--- client/src/screens/InformationScreen.js
+++ client/src/screens/InformationScreen.js
@@ -15,7 +15,7 @@ const InformationScreen = () => {
           User Manual
         </h4>
         <div className="card-body">
-          <h4>Welcome ! These are few guidelines for User.</h4>
+          <h4>Welcome! These are few guidelines for User.</h4>
           <div className="card-text">
             <div>
               <h5>1. Voter Registration</h5>
@@ -23,17 +23,17 @@ const InformationScreen = () => {
                 <li>
                   For casting the vote, student needs to first register to
                   participate in voting process. Student can access registration
-                  form in <strong> "Voter Registration" </strong>menu once the
+                  form in <strong> "Voter Verification" </strong> menu once the
                   registration phase starts.
                 </li>
                 <li>
                   Student can only register in the{" "}
-                  <strong> Registration Phase </strong>. After the registration
+                  <strong> Registration Phase</strong>. After the registration
                   phase is over, the student can not register and thus will not
                   be able to vote.
                 </li>
                 <li>
-                  For registration, the student will have to enter is PRN number
+                  For registration, the student will have to enter his PRN (Permanent Registration Number)
                   and Mobile Number.
                 </li>
               </ul>
@@ -45,7 +45,7 @@ const InformationScreen = () => {
                 Overall voting process is divided into four phases. All of which
                 will be initialized and terminated by the admin. <br />
                 User will have to participate in the process according to
-                current phase
+                current phase.
               </li>
               <ol className="list-group list-group-numbered">
                 <li className="list-group-item">
@@ -54,7 +54,7 @@ const InformationScreen = () => {
                   carried out.
                 </li>
                 <li className="list-group-item">
-                  <strong>Apply for candidate Phase: </strong>
+                  <strong>Candidate Application Phase: </strong>
                   After all students get registered for voting, verified
                   students by admin can apply as a candidate for ongoing
                   election role.
@@ -64,14 +64,14 @@ const InformationScreen = () => {
                   After initialization of voting phase from the admin, user can
                   cast the vote in voting section. The casting of vote can be
                   simply done by clicking on "VOTE" button next to chosen
-                  candidate. After which transaction will be initiated and on
-                  confirmation, the vote will get successfully casted. Students
-                  can vote only during voting phase is active.
+                  candidate. After which the transaction will be initiated and on
+                  confirmation, the vote will get casted successfully. Students
+                  can vote only when the voting phase is active.
                 </li>
                 <li className="list-group-item">
                   <strong>Result Phase: </strong>
-                  This is the final stage of whole voting process during which
-                  the results of election and summary statistics of election
+                  This is the final stage of whole voting procedure during which
+                  the results and analysis of election
                   will be displayed at "Result" section.
                 </li>
               </ol>
