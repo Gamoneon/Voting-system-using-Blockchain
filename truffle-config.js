54a1967e76a22d89718a26f093649eec191df2a7
sahil kavitake <sahir.kavitake202@gmail.com>
2022-05-11 11:29:37 +0530

    deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -12,6 +17,19 @@ module.exports = {
   networks: {
     develop: {
       port: 7545,
+    },
+
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `wss://ropsten.infura.io/ws/v3/5ca0f3d23cbf48998e2543ac1436344f`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
   },
   settings: {
